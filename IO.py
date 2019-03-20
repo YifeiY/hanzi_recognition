636eee8465434709af1ddcc4857f154d1001c8be
Yifei <yifei_yin@outlook.com>
2019-03-20 19:37:38 -0400

    File read in finished, dev dataset has unidentified characters while train and test dataset seem to be fine
    The validity of the data need to be verified in future processes

--- IO.py
+++ IO.py
@@ -9,76 +9,87 @@ class PotIO:
   train_filename = "1.0train-GB1.pot"
   test_fileName = "1.0test-GB1.pot"
   dev_filename = "001.pot"
-
+  dev_characters = None
   def __init__(self):
-    self.readFiles()
+    self.characters = self.readFiles()
 
 
+  def getSample(self,index):
+    return self.characters[index]
 
   def readFiles(self):
     byte_order = sys.byteorder
+    print("reading files, please wait...")
     test_data = []
     #train_file = open(self.train_filename,'r')
+    characters = []
 
-    with open(self.dev_filename, "rb") as f:
+    position = 0
+    with open(self.train_filename, "rb") as f:
+
       while True:
-        b_this_sample_size = f.read(2)
-
-        print("b this sample size",b_this_sample_size)
-        if b_this_sample_size == b'':
+        print("{:,}".format(position))
+        position += 1
+        sample_size = f.read(2)
+        if sample_size == b'':
           break
-        us_this_sample_size = self.h2ud(b_this_sample_size)
-        print("this sample byte size:",us_this_sample_size)
 
-        this_data = f.read(us_this_sample_size)
+        sample_size = struct.unpack("<H", sample_size)[0]
+        # print("sample size:",sample_size)
 
-        tag_code = this_data[:2]
-        tag_code = hex()
-        print("tag_code:",tag_code.hex(),"parsed",tag_code.decode("GBK"))
 
-        b_stroke_number = this_data[4:6]
-        print(b_stroke_number)
-        us_stroke_number = self.h2ud(b_stroke_number)
-        print("stroke number",us_stroke_number)
+        dword_code = f.read(2)
+        if dword_code[0] != 0:
+          dword_code = bytes((dword_code[1],dword_code[0]))
 
-        strokes = []
-        stroke = []
-        for i in range(0,us_stroke_number,4):
-          decoded = []
-          next = [this_data[6+i:6+i+2],this_data[6+i+2:6+i+4]]
-          decoded = [(int(next[0][0]),int(next[0][1])),(int(next[1][0]),int(next[1][1]))]
+        tag_code = struct.unpack(">H", dword_code)[0]
+        f.read(2) # next two hex are meaningless
+        # print(hex(tag_code))
+        try:  tag = struct.pack('>H', tag_code).decode("gb2312")[0]
+        except:
+          print("rip")
+          f.read(sample_size - 2)
+          continue
+        tag_code = hex(tag_code)
 
-          if decoded == [(255,255),(0,0)]: # end of stroke
-            strokes.append(stroke)
-            stroke = []
+        # print("tag code:",tag_code+',',"tag:",tag)
+
+        stroke_number = struct.unpack("<H",f.read(2))[0]
+        # print("stroke number:",stroke_number)
+
+        strokes_samples= []
+        stroke_samples = []
+        current_stroke_number = 0
+        next = b'\x00'
+        while next != (b'\xff\xff', b'\xff\xff'):
+          next = (f.read(2),f.read(2))
+          if next == (b'\xff\xff', b'\x00\x00'):
+            # print("stroke end")
+            strokes_samples.append(stroke_samples)
+            # print(strokes_samples)
+            stroke_samples = []
+            current_stroke_number += 1
           else:
-            stroke.append(decoded)
-          if decoded == [(255, 255), (255, 255)]:
-            break
-        print(len(strokes[0]))
-        for i in strokes:
-          print(i)
-
-        tag_code = 0
-        exit()
+            stroke_samples.append(((struct.unpack("<H",next[0])[0],struct.unpack("<H",next[1])[0])))
+            current_stroke_number = 0
+        characters.append(Sample(tag_code,tag,stroke_number,strokes_samples))
+    return characters
 
 
 
-  def h2ud(self,bs):
-    '''reverse and converts hex to unsigned int'''
-    bs = str(bs.hex())
-    val = 0
-    dict = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'a':10,'b':11,'c':12,'d':13,'e':14,'f':15}
-    for i in range(len(bs)):
-      val += dict[bs[i]] * (16 ** i)
-    return val
+
 
 
 class Sample:
 
-  def __init__(self,tag_code,stroke_number,stroke_data):
-
+  def __init__(self,tag_code,tag,stroke_number,stroke_data):
+    self.tag_code= tag_code
+    self.tag = tag
+    self.stroke_number =stroke_number
+    self.stroke_data = stroke_data
     return
+
+
 
 
 
